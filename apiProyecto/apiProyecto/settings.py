f63984c8878e68ec54180f8047b8fbea2dd891f3
Diana De Leon <dianaximeo@gmail.com>
2020-05-14 08:03:19 -0600

    ya encripta contraseña

--- apiProyecto/apiProyecto/settings.py
+++ apiProyecto/apiProyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'guardian',
+    'corsheaders',
+
     'clientes.apps.ClientesConfig',
     'compras.apps.ComprasConfig',
     'empleados.apps.EmpleadosConfig',
@@ -52,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +141,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3600),
+}
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'guardian.backends.ObjectPermissionBackend'
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
--- apiProyecto/usuarios/models.py
+++ apiProyecto/usuarios/models.py
@@ -16,7 +16,7 @@ class MiUsuarioAdministrador(BaseUserManager):
             raise ValueError("Los usuarios necesitan tener un tipo")
 
         user = self.model(
-            email.self.normalize_email(email),
+            email=self.normalize_email(email),
             username=username,
             tipo=tipo,
         )
@@ -28,13 +28,13 @@ class MiUsuarioAdministrador(BaseUserManager):
     def create_superuser(self, email, username, password):
         user = self.create_user(
             email=self.normalize_email(email),
-            password=password,
             username=username,
         )
 
         user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
